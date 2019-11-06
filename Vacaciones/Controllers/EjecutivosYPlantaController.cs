05f062236d4208f904c58e693e97c13801a4ceea
Daniel Sepulveda Galindo <dasega1990@gmail.com>
2019-11-06 08:19:47 -0500

    Calculo fecha fin en pantalla de Ejecutivos

--- Vacaciones/Controllers/EjecutivosYPlantaController.cs
+++ Vacaciones/Controllers/EjecutivosYPlantaController.cs
@@ -1,50 +1,88 @@
-﻿using Newtonsoft.Json;
+﻿using log4net;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Web.Mvc;
 using Vacaciones.Models.ModelosGenerales;
+using Vacaciones.Models.ModelosMotorDeReglas;
+using Vacaciones.Models.ModelosRespuestaSAP;
 using Vacaciones.Utilities;
+using Vacaciones.Utilities.IntegracionesServicios;
 
 namespace Vacaciones.Controllers
 {
     public class EjecutivosYPlantaController : Controller
     {
-
-        MensajeRespuesta oMensajeRespuesta = new MensajeRespuesta();
-        List<EmpleadoModels> oLstEmpleados = new List<EmpleadoModels>();
+        //Variable para almacenar los Log's
+        private static readonly ILog Logger = LogManager.GetLogger(Environment.MachineName);
 
         // GET: EjecutivosYPlanta
-        public ActionResult Index(string oDatosPersona, string oDatosFormulario, string oDatosSAP)
+        public ActionResult Index(string oDatosFormulario, string oDatosSAP)
         {
-            PersonaModels oPersona = new PersonaModels();
-            oPersona = JsonConvert.DeserializeObject<PersonaModels>(oDatosPersona);
+            try
+            {
+                RespuestaMotorModels oRespuestaMotor = new RespuestaMotorModels();
+                RespuestaSAPModels oRespuestaSAPModels = new RespuestaSAPModels();
 
-            ViewBag.NombreEmpleado = oPersona.Nombres + oPersona.Apellidos;
-            ViewBag.NumeroDias = oPersona.NumeroDias;
-            ViewBag.Documento = oPersona.Identificacion;
+                oRespuestaMotor = JsonConvert.DeserializeObject<RespuestaMotorModels>(oDatosFormulario);
+                oRespuestaSAPModels = JsonConvert.DeserializeObject<RespuestaSAPModels>(oDatosSAP);
+
+                ViewBag.Documento = oRespuestaSAPModels.Details[0].NroDocumento;
+
+                //Asignacion dle nombre del Empleado
+                ViewBag.NombreEmpleado = oRespuestaSAPModels.Details[0].PrimerNombre + " " + oRespuestaSAPModels.Details[0].SegundoNombre + " " +
+                                         oRespuestaSAPModels.Details[0].PrimerApellido + " " + oRespuestaSAPModels.Details[0].SegundoApellido;
+
+                //Falta definir el numero de dias
+                ViewBag.NumeroDias = 22;  // Pendiente por realizar ////////////////////////
 
 
-            // Se obtienen las fechas de los festivos, sabados y domingos (Si se envía true incluira los sábados, si se envía false no incluirá los sábados, según criterio)
-            string DiasFestivosSabadosDomingos = FestivosColombia.DiasFestivoSabadosDomingosConcatenado(DateTime.Now.Year, true);
-            ViewBag.DiasFestivosSabadosDomingos = DiasFestivosSabadosDomingos;
+                foreach (var oReglas in oRespuestaMotor.Reglas)
+                {
+                    switch (oReglas.Prmtro)
+                    {
+                        case "NroMinDias":
+                            ViewBag.MinimoDias = Convert.ToDouble(oReglas.Vlr_Slda);
+                            break;
 
-            // Fecha donde el usuario puede iniciar sus vacaciones (Se le agregan los días según condicion, 1 o 15 días)
-            ViewBag.InicioFecha = DateTime.Now.AddDays(15);
+                        case "DiasMinCalendario":
+                            ViewBag.InicioFecha = DateTime.Now.AddDays(Convert.ToDouble(oReglas.Vlr_Slda));
+                            break;
+
+                        case "DiasMaxCalendario":
+                            ViewBag.FinFecha = DateTime.Now.AddDays(Convert.ToDouble(oReglas.Vlr_Slda));
+                            break;
+
+                    }
+                }
+
+                ViewBag.SabadoHabil = oRespuestaSAPModels.Details[0].SabadoHabil;
+
+                // Se obtienen las fechas de los festivos, sabados y domingos (Si se envía true incluira los sábados, si se envía false no incluirá los sábados, según criterio)
+                string DiasFestivosSabadosDomingos = FestivosColombia.DiasFestivoSabadosDomingosConcatenado(DateTime.Now.Year, oRespuestaSAPModels.Details[0].SabadoHabil == "NO" ? true : false);
+                ViewBag.DiasFestivosSabadosDomingos = DiasFestivosSabadosDomingos;
 
 
-            // Fecha maxima que se le mostrará al usuario para pedir sus vacaciones (Esta para 60 días se puede cambiar según criterio)
-            ViewBag.FinFecha = DateTime.Now.AddDays(60);
-            return View();
+                return View();
+            }
+            catch (Exception Ex)
+            {
+                return null;
+            }
         }
 
         public JsonResult ValidarCantidadDias(int NumeroDias, float NumDiasDisponibles)
         {
+            MensajeRespuesta oMensajeRespuesta = new MensajeRespuesta();
+
             if (NumeroDias < 6)
             {
                 oMensajeRespuesta = new MensajeRespuesta
                 {
                     Codigo = "1",
-                    Mensaje = "La cantidad de días debe ser superior a 6.",
+                    Mensaje = "La cantidad de días debe ser superior a",
                     Resultado = Json("", JsonRequestBehavior.AllowGet)
                 };
             }
@@ -53,8 +91,8 @@ namespace Vacaciones.Controllers
             {
                 oMensajeRespuesta = new MensajeRespuesta
                 {
-                    Codigo = "1",
-                    Mensaje = "La cantidad de días debe ser menor o igual al número de días disponibles.",
+                    Codigo = "2",
+                    Mensaje = "La cantidad de días debe ser menor o igual al número de días disponibles",
                     Resultado = Json("", JsonRequestBehavior.AllowGet)
                 };
             }
@@ -65,6 +103,8 @@ namespace Vacaciones.Controllers
 
         public JsonResult AgregarEmpleado(string Cedula, string NumeroDias, string FechaInicio, string FechaFin)
         {
+            MensajeRespuesta oMensajeRespuesta = new MensajeRespuesta();
+
             oMensajeRespuesta = new MensajeRespuesta
             {
                 Codigo = "1",
@@ -76,5 +116,103 @@ namespace Vacaciones.Controllers
 
         }
 
+
+        public JsonResult CalcularFechaFin(int NumeroDias, string FechaInicio, string SabadoHabil, string DiasFestivosSabadosDomingos)
+        {
+            MensajeRespuesta oMensajeRespuesta = new MensajeRespuesta();
+            try
+            {
+                //string DiasFestivosSabadosDomingos = FestivosColombia.DiasFestivoSabadosDomingosConcatenado(DateTime.Now.Year, SabadoHabil == "NO" ? true : false);
+                DateTime FechaFin = Convert.ToDateTime(FechaInicio).AddDays(NumeroDias);
+                int contador = 0;
+                string[] Fechas;
+                Fechas = DiasFestivosSabadosDomingos.Split(',');
+                foreach (var item in Fechas)
+                {
+                    string[] DatosFechaItem = item.Split('/');
+
+                    var FechaItem = new DateTime(Convert.ToInt32(DatosFechaItem[2]), Convert.ToInt32(DatosFechaItem[0]), Convert.ToInt32(DatosFechaItem[1])).ToShortDateString();
+
+
+                    if (Convert.ToDateTime(FechaItem) >= Convert.ToDateTime(FechaInicio) && Convert.ToDateTime(FechaItem) <= FechaFin)
+                        contador++;
+                }
+
+                FechaFin = CalcularFechaFinHabil(Fechas, Convert.ToDateTime(FechaInicio), FechaFin, NumeroDias, NumeroDias - contador);
+
+                oMensajeRespuesta.Codigo = "0";
+                oMensajeRespuesta.Mensaje = "";
+                oMensajeRespuesta.Resultado = Json(FechaFin.ToShortDateString());
+
+                return Json(oMensajeRespuesta, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception Ex)
+            {
+                Logger.Error("Ocurrió un error calculando la fecha de fin. Fecha de inicio: " +
+                   FechaInicio + ". Número de días: " + NumeroDias +
+                   ". Exception: " + Ex);
+
+                oMensajeRespuesta.Codigo = "-1";
+                oMensajeRespuesta.Mensaje = "Ocurrió un error inesperado. Consulte al administrador del sistema.";
+                oMensajeRespuesta.Resultado = Json(DateTime.Now.ToShortDateString(), JsonRequestBehavior.AllowGet);
+
+                return Json(oMensajeRespuesta, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public DateTime CalcularFechaFinHabil(string[] Fechas, DateTime FechaInicio, DateTime FechaFin, int NumeroDias, int NumeroDiasHabiles)
+        {
+            MensajeRespuesta oMensajeRespuesta = new MensajeRespuesta();
+
+            try
+            {
+                TimeSpan tSpan = new TimeSpan();
+                int contador = 0;
+
+                foreach (var item in Fechas)
+                {
+                    string[] DatosFechaItem = item.Split('/');
+
+                    var FechaItem = new DateTime(Convert.ToInt32(DatosFechaItem[2]), Convert.ToInt32(DatosFechaItem[0]), Convert.ToInt32(DatosFechaItem[1])).ToShortDateString();
+
+                    if (Convert.ToDateTime(FechaItem) == FechaFin)
+                    {
+                        FechaFin = FechaFin.AddDays(1);
+                        tSpan = FechaFin - FechaInicio;
+                    }
+
+                    if (Convert.ToDateTime(FechaItem) >= Convert.ToDateTime(FechaInicio) && Convert.ToDateTime(FechaItem) <= FechaFin)
+                        contador++;
+
+                }
+
+                TimeSpan oCalculo = FechaFin - FechaInicio;
+                int Resultado = oCalculo.Days - contador;
+
+                if (Resultado < NumeroDias)
+                {
+                    FechaFin = FechaFin.AddDays(1);
+                    FechaFin = CalcularFechaFinHabil(Fechas, FechaInicio, FechaFin, NumeroDias, Resultado);
+
+                }
+
+                return FechaFin;
+
+            }
+            catch (Exception Ex)
+            {
+                Logger.Error("Ocurrió un error calculando la fecha de fin. Fecha de inicio: " +
+                  FechaInicio + ". Número de días: " + NumeroDias +
+                  ". Exception: " + Ex);
+
+                oMensajeRespuesta.Codigo = "-1";
+                oMensajeRespuesta.Mensaje = "Ocurrió un error inesperado. Consulte al administrador del sistema.";
+                oMensajeRespuesta.Resultado = Json("", JsonRequestBehavior.AllowGet);
+
+                return DateTime.Now;
+            }
+
+        }
     }
+
 }
