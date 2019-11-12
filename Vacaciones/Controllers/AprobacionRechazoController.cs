2aafb60c0771f9b0b1abd406b39f710af5e429d4
visualmvm <visualmvm@mvmingenieriadesoftwareco.onmicrosoft.com>
2019-11-12 17:21:16 -0500

    AprobacionRechazo

---
+++ Vacaciones/Controllers/AprobacionRechazoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vacaciones.Controllers
+{
+    public class AprobacionRechazoController : Controller
+    {
+        // GET: AprobacionRechazo
+        public ActionResult Index(int consecutivo, string correo_jefe)
+        {
+            
+            return View();
+        }
+    }
+}
--- Vacaciones/Controllers/HomeController.cs
+++ Vacaciones/Controllers/HomeController.cs
@@ -76,7 +76,5 @@ namespace Vacaciones.Controllers
                 return Json(oMensajeRespuesta, JsonRequestBehavior.AllowGet);
             }
         }
-
-
     }
 }
