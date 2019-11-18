e9aa4461a67744e788b4e8df0bd15472549ee43a
Cesar Augusto Montoya Gonzalez <cesar.montoya@mvm.com.co>
2019-11-18 09:56:39 -0500

    Se agrega el correo del anotador, probar desde el pc de Daniel dado que no tengo VPN

--- Vacaciones/Models/ModelosAproRechazo/SolicitudAR.cs
+++ Vacaciones/Models/ModelosAproRechazo/SolicitudAR.cs
@@ -16,6 +16,7 @@ namespace Vacaciones.Models.ModelosAproRechazo
         public DateTime fcha_fn_vcc { get; set; }
         public string crreo_slctnte { get; set; }
         public string idntfccn_slctnte { get; set; }
+        public string crro_antdr { get; set; }
     }
 
     public class ResultadoCambioEstado
